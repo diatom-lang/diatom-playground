9497bde38fce93aca222e74d3875815cf3618cb5
TerenceNg03 <stoicism03@gmail.com>
2022-12-16 11:43:53 +0800

    Update to 0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,16 @@ use diatom::Parser;
 
 #[wasm_bindgen]
 pub fn get_version() -> String{
-    todo!()
+    diatom::VERSION.to_string()
 }
 
 #[wasm_bindgen]
 pub fn compile(code: &str) -> String {
     let mut parser = Parser::new();
-    parser.parse_str(OsStr::new("playground"), code);
-    let result = if parser.diagnostic_count() > 0 {
-        parser.render_diagnoses(true)
-    } else {
-        let ast = parser.get_incremental();
-        format!("{:?}", ast.collect::<Vec<_>>())
-    };
+    let ast = parser.parse_str(OsStr::new("playground"), code);
+    let mut result = ast.diagnoser.render(true);
+    if ast.diagnoser.error_count() == 0 {
+        result += &format!("{:?}", ast.statements);
+    }
     convert(&result, true, true).unwrap_or_else(|_|"Internal error: Failed to convert ansi output".to_string())
 }
