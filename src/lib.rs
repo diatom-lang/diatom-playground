22d378f1f7cf3244cc6f190cc4e0831c2ec7f655
TerenceNg03 <stoicism03@gmail.com>
2023-01-31 00:04:16 +0800

    Upgrade to v0.4.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,18 @@
-use std::ffi::OsStr;
-
-use wasm_bindgen::prelude::*;
 use ansi_to_html::convert;
-use diatom::Parser;
+use diatom::Interpreter;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn get_version() -> String{
+pub fn get_version() -> String {
     diatom::VERSION.to_string()
 }
 
 #[wasm_bindgen]
 pub fn compile(code: &str) -> String {
-    let mut parser = Parser::new();
-    let ast = parser.parse_str(OsStr::new("playground"), code);
-    let mut result = ast.diagnoser.render(true);
-    if ast.diagnoser.error_count() == 0 {
-        result += &format!("{:?}", ast.statements);
-    }
-    convert(&result, true, true).unwrap_or_else(|_|"Internal error: Failed to convert ansi output".to_string())
+    let mut interpreter = Interpreter::new();
+    let result = match interpreter.exec(code, true) {
+        Ok(s) | Err(s) => s,
+    };
+    convert(&result, true, true)
+        .unwrap_or_else(|_| "Internal error: Failed to convert ansi output".to_string())
 }
