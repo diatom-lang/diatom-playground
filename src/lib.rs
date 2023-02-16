83b52d71f167ae7b190271aa04f2175b9597fe9a
TerenceNg03 <stoicism03@gmail.com>
2023-02-16 12:09:58 +0800

    Update to v0.4.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::ffi::OsStr;
+
 use ansi_to_html::convert;
-use diatom::Interpreter;
+use diatom::{Interpreter, IoWrite};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -7,11 +9,30 @@ pub fn get_version() -> String {
     diatom::VERSION.to_string()
 }
 
+struct JsOutputStream<'a> {
+    callback: &'a js_sys::Function,
+}
+
+impl<'a> IoWrite for JsOutputStream<'a> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let parameter = JsValue::from_str(
+            std::str::from_utf8(buf).unwrap_or("Output is not valid utf8 string\n"),
+        );
+        let this = JsValue::null();
+        let _ = self.callback.call1(&this, &parameter);
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
 #[wasm_bindgen]
-pub fn compile(code: &str) -> String {
-    let mut interpreter = Interpreter::new();
-    let result = match interpreter.exec(code, true) {
-        Ok(s) | Err(s) => s,
+pub fn exec(code: &str, f: &js_sys::Function) -> String {
+    let stream = JsOutputStream { callback: f };
+    let mut interpreter = Interpreter::new(stream);
+    let result = match interpreter.exec(code, OsStr::new("<playground>"),true) {
+        Ok(_) => return String::new(),
+        Err(s) => s,
     };
     convert(&result, true, true)
         .unwrap_or_else(|_| "Internal error: Failed to convert ansi output".to_string())
